3dae889878516317acde98189a66ae1b42e1964a
Wildur13 <williamswakam09@gmail.com>
2021-01-07 20:11:18 +0100

    modifications

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY == 0){
+            $('.social-2').addClass("soc2");
+        }else{
+            $('.social-2').removeClass("soc2");
+        }
         
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
+            $('.social').addClass("soc");
         }else{
             $('.scroll-up-btn').removeClass("show");
+            $('.social').removeClass("soc");
         }
     });
 
